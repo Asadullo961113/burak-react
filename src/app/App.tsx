9f3161eb4d896ba1e078ed47dfd916951379f4e5
Asadullo961113 <asadillok9@gmail.com>
2024-10-02 23:28:00 +0900

    Feat: initial setup

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+ import React, { useState } from 'react';
+import logo from './logo.svg';
+import '../css/app.css';
+
+function App() {
+  return <div> hello Asadullo </div>
+}
+
+export default App;
