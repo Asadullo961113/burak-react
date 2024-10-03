87d3ac67153ffdd60ba2fc223c4abb415dfdb9b6
Asadullo961113 <asadillok9@gmail.com>
2024-10-03 18:40:18 +0900

    fix: MUI cutomized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,24 @@
  import React, { useState } from 'react';
 import logo from './logo.svg';
 import '../css/app.css';
+import {Box,Button,Container,Stack, Typography} from "@mui/material"
+import { RippleBadge } from './material/MaterialTheme/styled';
 
 function App() {
-  return <div> hello Asadullo </div>
+  return <Container sx={{background: "orange"}}>
+    <Stack flexDirection={"column"} >
+      <Box sx={{my:4}}>
+        <Typography variant='h4' component={"h4"}>
+          Create React App on Typscript and Redux
+        </Typography>
+      </Box>
+      <Box>
+        <RippleBadge badgeContent={4}>
+        <Button variant="contained">Contained</Button>
+        </RippleBadge>
+      </Box>
+    </Stack>
+  </Container>
 }
 
 export default App;
