bc69b61c809f04b26503e3b59bc242c0dd992d5e
Asadullo961113 <asadillok9@gmail.com>
2024-10-05 00:16:43 +0900

    feat:Develop Screen Based Components

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './app/material/MaterialTheme';
 import './css/index.css';
+import {BrowserRouter as Router} from "react-router-dom";
 
   
 ReactDOM.render(
@@ -15,7 +16,9 @@ ReactDOM.render(
     <Provider store={store}>
     <ThemeProvider theme={theme}> 
        <CssBaseline /> 
-       <App />
+       <Router>
+         <App />
+       </Router>
     </ThemeProvider>
     </Provider>
   </React.StrictMode>,
