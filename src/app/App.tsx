3f6a39dcbb3e9f7c8897bc9dbf45452945c8fc72
Asadullo961113 <asadillok9@gmail.com>
2024-10-06 16:30:14 +0900

    fix: Organized Header and Footer Components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,36 +2,23 @@
 import logo from './logo.svg';
 import '../css/app.css';
 import {Box,Button,Container,Stack, Typography} from "@mui/material"
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from './screens/homePage';
 import { ProductsPage } from './screens/productsPage';
 import { OrdersPage } from './screens/ordersPage';
 import { UserPage } from './screens/userPage';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { Footer } from './components/footer';
+
 
 function App() {
-  return <div>
-  <nav>
-    <ul>
-    <li>
-        <Link to="/">HomePage</Link>
-      </li>
-      <li>
-        <Link to="/products">ProductsPage</Link>
-      </li>
-      <li>
-        <Link to="/orders">OrdersPage</Link>
-      </li>
-      <li>
-        <Link to="/member-page">UserPage</Link>
-      </li>
-      <li>
-        <Link to="/">HomePage</Link>
-      </li>
-    </ul>
-  </nav>
 
+  const location = useLocation();
+  console.log("location: ", location)
 
-
+  return <>
+  {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar/>}
   <Switch>
     <Route path="/products">
       <ProductsPage />
@@ -46,7 +33,8 @@ function App() {
       <HomePage />
     </Route>
   </Switch>
-</div>
+  <Footer/>
+</>
 }
 
 
