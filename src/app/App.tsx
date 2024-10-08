1600756708686dca222ec9bd33c684a3246a91ba
Asadullo961113 <asadillok9@gmail.com>
2024-10-08 19:32:31 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
  import React, { useState } from 'react';
-import logo from './logo.svg';
-import '../css/app.css';
 import {Box,Button,Container,Stack, Typography} from "@mui/material"
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import { HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage';
-import { OrdersPage } from './screens/ordersPage';
-import { UserPage } from './screens/userPage';
-import { OtherNavbar } from './components/headers/OtherNavbar';
-import { HomeNavbar } from './components/headers/HomeNavbar';
-import { Footer } from './components/footer';
+import  HomePage  from './screens/homePage';
+import  ProductsPage from './screens/productsPage';
+import  OrdersPage  from './screens/ordersPage';
+import  UserPage  from './screens/userPage';
+import  OtherNavbar  from './components/headers/OtherNavbar';
+import  HomeNavbar  from './components/headers/HomeNavbar';
+import  Footer  from './components/footer';
+import  HelpPage  from './screens/helpPage';
+import '../css/app.css';
+import '../css/navbar.css';
+import '../css/footer.css';
+
 
 
 function App() {
@@ -28,6 +31,9 @@ function App() {
     </Route>
     <Route path="/member-page">
       <UserPage />
+    </Route>
+    <Route path="/help">
+      <HelpPage />
     </Route>
     <Route path="/">
       <HomePage />
