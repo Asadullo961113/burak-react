ea2aa98dca22af391a053181dcf2383829e4d697
Asadullo961113 <asadillok9@gmail.com>
2024-10-10 19:15:59 +0900

    feat: Develop Homepage

---
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -0,0 +1,53 @@
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+const activeUsers = [
+  {productName: "Martin", imagePath: "/img/martin.webp"},
+  {productName: "Justin", imagePath: "/img/justin.webp"},
+  {productName: "Rose", imagePath: "/img/rose.webp"},
+  {productName: "Nusret", imagePath: "/img/nusret.webp"}
+];
+
+export default function ActiveUsers() {
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                 
+                  return (
+                    <Card
+                      key={index}
+                      variant="outlined"
+                      className={"card"}
+                    >
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={ele.imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow>
+                        <Typography className={"member-nickname"}>
+                          {ele.productName}
+                        </Typography>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
